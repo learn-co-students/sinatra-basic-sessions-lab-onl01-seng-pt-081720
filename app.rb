afc83600ec446e3e7dc48e1a1395b15cd5503bc9
Cloudymolecule <baires82@gmail.com>
2020-11-04 11:15:20 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    configure do
+        enable :sessions
+        set :session_secret, "no_one_knows_the_meaning_of_this_2020"
+    end
+
+    get "/" do
+        erb :index
+    end
+
+    post "/checkout" do
+        session["item"] = params["item"]
+        @session = session
+        erb :checkout
+    end
 end
