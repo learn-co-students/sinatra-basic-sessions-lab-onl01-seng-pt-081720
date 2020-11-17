4630b0aa2157c1e2f1307f35539f4931de7adcb3
HaidyF <69495135+HaidyF@users.noreply.github.com>
2020-11-17 01:01:51 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "masr"
+      end
+
+      get '/' do
+        erb :index
+      end
+
+      get '/hey' do 
+        @session = session
+      end
+
+      post '/checkout' do
+        item=params["item"]
+        session["item"]=item
+        @session = session
+        
+      end
+      
 end
