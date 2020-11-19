c238fdc26d8479a7f2d4d42d6d145601fe66c914
jasonemarkson <jasonmarkson76@gmail.com>
2020-11-19 18:50:21 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :sessions_secret, "orange"
+    end
+
+    get '/' do
+        @session = session
+        erb :index
+    end
+    
+    post '/checkout' do
+        #binding.pry
+        @orange = session
+        session[:item] = params[:item]
+    end
 end
