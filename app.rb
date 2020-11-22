12f7a6280311f9656feb48685d2b5230b6dc4903
Angelica203 <brantleyhomebuyer@gmail.com>
2020-11-22 17:15:49 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    configure do
+        enable :sessions
+        set :session_secret, "supermom"
+    end
+    
+    get '/' do
+        erb :index
+    end
+    
+    post '/checkout' do
+        @item = session
+        @item[:item] = params[:item]
+        @item[:item]
+    end
+
 end
